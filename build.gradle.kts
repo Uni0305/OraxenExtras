d388d7e798ca0576fd2817a0d39f1965dff25bbe
Uni0305 <git@uni0305.me>
2024-10-24 19:05:25 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("idea")
+    kotlin("jvm") version "2.0.21"
+    id("co.uzzu.dotenv.gradle") version "4.0.0"
+    id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.2.0"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
+}
+
+group = "me.uni0305"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.oraxen.com/releases")
+}
+
+dependencies {
+    compileOnly(kotlin("test"))
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("io.th0rgal:oraxen:1.183.0")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+bukkitPluginYaml {
+    main = "me.uni0305.oraxen.OraxenExtrasPlugin"
+    apiVersion = "1.20"
+    author = "Uni0305"
+    description = "A plugin that adds extra features to Oraxen"
+    depend = listOf("Oraxen")
+    libraries = listOf(
+        "org.jetbrains.kotlin:kotlin-stdlib:2.0.21",
+    )
+}
+
+tasks.jar {
+    if (env.isPresent("JAR_DIR"))
+        destinationDirectory.set(file(env.fetch("JAR_DIR")))
+}
+
+tasks.runServer {
+    minecraftVersion("1.20.4")
+    if (env.isPresent("SERVER_JAR"))
+        serverJar(file(env.fetch("SERVER_JAR")))
+}
---
+++ src/main/kotlin/me/uni0305/oraxen/OraxenExtrasPlugin.kt
@@ -0,0 +1,5 @@
+package me.uni0305.oraxen
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class OraxenExtrasPlugin : JavaPlugin()
