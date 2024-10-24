b6eb48592e76246f2eaedf398ffd83d3877b6365
Uni0305 <git@uni0305.me>
2024-10-24 20:01:38 +0900

    Feat: Implement plugin initialization

--- src/main/kotlin/me/uni0305/oraxen/OraxenExtrasPlugin.kt
+++ src/main/kotlin/me/uni0305/oraxen/OraxenExtrasPlugin.kt
@@ -1,5 +1,24 @@
 package me.uni0305.oraxen
 
+import me.uni0305.oraxen.config.ExternalPackImportConfig
+import me.uni0305.oraxen.service.ExternalPackImportService
 import org.bukkit.plugin.java.JavaPlugin
 
-class OraxenExtrasPlugin : JavaPlugin()
+class OraxenExtrasPlugin : JavaPlugin() {
+    companion object {
+        lateinit var externalPackImportService: ExternalPackImportService private set
+    }
+
+    override fun onEnable() {
+        saveDefaultConfig()
+        reloadConfig()
+
+        val externalPackImportConfig = ExternalPackImportConfig(config)
+        externalPackImportService = ExternalPackImportService(externalPackImportConfig)
+        OraxenExtrasCommand(this)
+    }
+
+    override fun onDisable() {
+        saveConfig()
+    }
+}
